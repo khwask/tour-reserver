5c138450e485d4c935e348ff9065432688c15fe9
kawasakimsh <kawasakimsh@nttdata.co.jp>
2017-12-21 17:06:29 +0900

    first commit

---
+++ src/main/java/com/example/tourreserver/TourReserverApplication.java
@@ -0,0 +1,12 @@
+package com.example.tourreserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourReserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourReserverApplication.class, args);
+	}
+}
